d192f842c3696b76e4382191728f6e954c12312a
Ross Hadden <rosshadden@gmail.com>
2011-12-04 00:18:04 -0500

    Express. converted from my own router.js to using Express; added README.md;

---
+++ README.md
@@ -0,0 +1,3 @@
+Boilerplate for future node.js projects.
+
+Hopefully I can just clone this whenever I want to start a new project.
--- server.js
+++ server.js
@@ -1,24 +1,48 @@
-var http = require('http'),
-	fs = require('fs'),
-	util = require('util'),
-	router = require('./router'),
-	io = require('socket.io'),
+var express = require('express'),
+	app = express.createServer(),
+	parseURL = require('url').parse,
+	/*	SOCKET.IO
+			io = require('socket.io'),
+	*/
 	
-	clients = [],
+	clients = {},
 	
-	start = function(){
-		var server = http.createServer(function(request,response){
-				var route = router.route(request.url),
-					rs = fs.createReadStream(route.path,response);
-				response.writeHead(200,{
-					'Content-Type':	route.mime
-				});
-				util.pump(rs,response);
-			}),
-			socket = io.listen(server);
-		socket.set('log level',1);
-		server.listen(+(process.argv[2] || 80));
-		socket.sockets.on('connection',function(client){
+	init = function(){
+		app.configure(function(){
+			app.set('views',__dirname + '/views');
+			app.set('view engine','jade');
+			app.use(express.bodyParser());
+			app.use(app.router);
+			app.use(express.static(__dirname + '/ink'));
+		});
+		app.listen(+(process.argv[2] || 80));
+		console.log('http://localhost:%d | %s',+(process.argv[2] || 80),app.settings.env);
+	},
+	serve = function(){
+		init();
+		/*	SOCKET.IO
+				var io = require('socket.io').listen(app);
+				io.set('log level',1);
+				socket.sockets.on('connection',function(socket){});
+		*/
+		
+		app.get('/',function(request,response){
+			response.render('index');
+		});
+		app.get('/GET',function(request,response){
+			var data = '',
+				url = parseURL(request.url,true);
+			response.contentType('application/json');
+			
+			switch(url.query.on){
+				case 'debug':
+					data = 'debug';
+					break;
+				default:
+					data = 'default';
+			}
+			
+			response.end(JSON.stringify(data));
 		});
 	};
-exports.start = start;
+exports.serve = serve;
